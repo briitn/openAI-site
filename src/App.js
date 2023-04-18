3ae5d46fd5a101b61276de34ce66b97c6688c6d6
princegobah <babynamek@gmail.com>
2023-04-18 14:31:09 -0400

    style

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { Brand, Cta, Navbar } from './components';
+import { Blog, Features, Header, Footer, Possibility, WhatGPT3 } from './containers';
+function App() {
+  return (
+    <div className="App">
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+        </div>
+  <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
